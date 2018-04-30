d6d97210d34322dec1930aee8ad0b91cafbfa354
Mert Artan <ondermertartan@gmail.com>
2018-04-30 19:17:01 +0300

    tasarım eklendi

---
+++ application/views/footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+
+ <script src="<?=site_url('vendor/jquery/jquery.min.js')?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?=site_url('vendor/bootstrap/js/bootstrap.min.js')?>"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="<?=site_url('vendor/metisMenu/metisMenu.min.js')?>"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="<?=site_url('dist/js/sb-admin-2.js')?>"></script>
+
+    </body>
+
+</html>
--- application/views/panel/login.php
+++ application/views/panel/login.php
@@ -15,13 +15,13 @@
     <link href="<?=site_url('vendor/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="<?=site_url('vendor/metisMenu/metisMenu.min.css')?> rel="stylesheet">
+    <link href="<?=site_url('vendor/metisMenu/metisMenu.min.css')?>" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="<?=site_url('dist/css/sb-admin-2.css')?>" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="<?=site_url('vendor/font-awesome/css/font-awesome.min.css')?>" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -66,16 +66,16 @@
     </div>
 
     <!-- jQuery -->
-    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="<?=site_url('vendor/jquery/jquery.min.js')?>"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?=site_url('vendor/bootstrap/js/bootstrap.min.js')?>"></script>
 
     <!-- Metis Menu Plugin JavaScript -->
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+    <script src="<?=site_url('vendor/metisMenu/metisMenu.min.js')?>"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="../dist/js/sb-admin-2.js"></script>
+    <script src="<?=site_url('dist/js/sb-admin-2.js')?>"></script>
 
 </body>
 
