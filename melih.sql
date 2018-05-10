95923ebb93e651c2dace95c9a73dbf559a9bb8f6
sezer doğru <sezer.dogru91@gmail.com>
2018-05-10 11:42:16 +0300

    siparisler

--- melih.sql
+++ melih.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 03 May 2018, 19:39:52
+-- Üretim Zamanı: 10 May 2018, 10:42:00
 -- Sunucu sürümü: 10.1.16-MariaDB
 -- PHP Sürümü: 5.5.38
 
@@ -38,10 +38,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`, `level`) VALUES
-(1, 'admin', '1', 1),
-(3, 'admin3', '1', 1),
-(4, 'admin5', '1', 1),
-(6, 'mert', '1', 2);
+(1, 'admin', '1', 1);
 
 -- --------------------------------------------------------
 
@@ -53,21 +50,9 @@ CREATE TABLE `baskets` (
   `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `product_id` int(11) DEFAULT NULL,
-  `quantity` int(11) DEFAULT NULL
+  `quantity` int(11) DEFAULT NULL,
+  `state` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Tablo döküm verisi `baskets`
---
-
-INSERT INTO `baskets` (`id`, `user_id`, `product_id`, `quantity`) VALUES
-(2, 1, 1, 1),
-(7, 3, 2, 1),
-(9, 4, 1, 1),
-(11, 8, 1, 1),
-(16, 8, 5, 1),
-(17, 16, 7, 1),
-(18, 3, 8, 1);
 
 -- --------------------------------------------------------
 
@@ -91,10 +76,7 @@ CREATE TABLE `bill` (
 --
 
 INSERT INTO `bill` (`id`, `user_id`, `code`, `country`, `state`, `phone`, `address`, `detail`) VALUES
-(17, 1, 16800, 0, 0, 5362083578, 'asd', 'denemed'),
-(18, 3, 16800, 0, 0, 0, 'bursa', ''),
-(19, 4, 16800, 0, 0, 0, 'bursa', ''),
-(20, 8, 16800, 0, 0, 0, 'bursa', '');
+(1, 21, 0, 0, 0, 0, 'dsdf', '');
 
 -- --------------------------------------------------------
 
@@ -113,9 +95,8 @@ CREATE TABLE `brands` (
 --
 
 INSERT INTO `brands` (`id`, `name`, `category_id`) VALUES
-(1, 'nike', 1),
-(2, '1', 1),
-(6, 'asus', 6);
+(1, '11', 1),
+(2, '21', 2);
 
 -- --------------------------------------------------------
 
@@ -133,11 +114,8 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`) VALUES
-(0, 'kid'),
-(1, 'spor'),
-(2, '1'),
-(4, 'Bilgisayar'),
-(6, 'Bilgisayar');
+(1, '1'),
+(2, '2');
 
 -- --------------------------------------------------------
 
@@ -153,15 +131,6 @@ CREATE TABLE `comments` (
   `comment_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Tablo döküm verisi `comments`
---
-
-INSERT INTO `comments` (`id`, `comment`, `product_id`, `user_id`, `comment_date`) VALUES
-(1, 'sadasd', 5, 8, NULL),
-(13, 'sads', 8, 3, NULL),
-(15, 'sad', 8, 3, NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -170,12 +139,17 @@ INSERT INTO `comments` (`id`, `comment`, `product_id`, `user_id`, `comment_date`
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
-  `product_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `date` date DEFAULT NULL,
-  `piece` int(11) DEFAULT NULL,
-  `state` int(1) DEFAULT '0'
+  `total` int(11) DEFAULT '0',
+  `user_id` int(11) NOT NULL,
+  `state` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `orders`
+--
+
+INSERT INTO `orders` (`id`, `total`, `user_id`, `state`) VALUES
+(1, 400, 21, 1);
 
 -- --------------------------------------------------------
 
@@ -211,8 +185,7 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `name`, `price`, `category_id`, `brand_id`, `sold`, `type`, `image`, `detail`) VALUES
-(7, 'deneme', 0, 1, 1, 0, 'Cep Telefonu', 'product12.jpg', 'aslkdsjlkd'),
-(8, 'bebek arabası', 100, 0, 1, 0, NULL, 'product12.jpg', 'sdsadklasdj');
+(17, '1', 400, 1, 1, 0, NULL, '1.jpg', '1');
 
 -- --------------------------------------------------------
 
@@ -238,28 +211,17 @@ CREATE TABLE `users` (
   `surname` varchar(20) DEFAULT NULL,
   `password` varchar(6) DEFAULT NULL,
   `email` text,
-  `username` varchar(10) DEFAULT NULL
+  `username` varchar(10) DEFAULT NULL,
+  `banned` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Tablo döküm verisi `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `surname`, `password`, `email`, `username`) VALUES
-(1, 'Sezer', 'DOĞRU', '11', 'sezer1@gmail.com', 'casablanka'),
-(3, 'test', 'doğru', '1', 'test@test.com', 'sezer'),
-(4, 'deneme', 'doğru', '1', 'test2@gmail.com', 'sezer1'),
-(5, 'sad', 'dsa', 'sa', 'asd@dsad.cp', 'sas'),
-(6, 'asdasd', 'asds', 'asdsad', 'dasdsa@dsa.com', 'asd'),
-(7, 'asdsa', NULL, 'sd', 'sd@f.com', NULL),
-(8, 'isim', '', '', '1@1.com', ''),
-(12, 'asd', 'dsa', 'd', 'zdlkjflkf@asd.com', 'adsd'),
-(13, 'sads', NULL, 'asdasd', 's@1.com', NULL),
-(14, 'met', 'ds', '1', 'sads@ds.dcom', 'sasas'),
-(15, 'deneme', 'deneme', 'deneme', 'deneme@deneme.com', 'deneme'),
-(16, 'sadlksj', NULL, '1', 'test@1.com', NULL),
-(17, 'net', 'sdd', 'dasda', 'aasds@dsadsa.com', '111111'),
-(18, 'adaksjdhjk', NULL, 'asdasd', 'sadas@asd.com', NULL);
+INSERT INTO `users` (`id`, `name`, `surname`, `password`, `email`, `username`, `banned`) VALUES
+(20, 'deneme', 'deneme', 'deneme', 'denem@sdenee.com', 'deneme', 1),
+(21, 'ads', NULL, '1', '1@1.com', NULL, 0);
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -339,37 +301,37 @@ ALTER TABLE `users`
 -- Tablo için AUTO_INCREMENT değeri `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Tablo için AUTO_INCREMENT değeri `baskets`
 --
 ALTER TABLE `baskets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Tablo için AUTO_INCREMENT değeri `bill`
 --
 ALTER TABLE `bill`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Tablo için AUTO_INCREMENT değeri `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Tablo için AUTO_INCREMENT değeri `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Tablo için AUTO_INCREMENT değeri `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Tablo için AUTO_INCREMENT değeri `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Tablo için AUTO_INCREMENT değeri `payment`
 --
@@ -379,7 +341,7 @@ ALTER TABLE `payment`
 -- Tablo için AUTO_INCREMENT değeri `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- Tablo için AUTO_INCREMENT değeri `stok`
 --
@@ -389,7 +351,7 @@ ALTER TABLE `stok`
 -- Tablo için AUTO_INCREMENT değeri `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
